59d919e37a6ecd826a84495ace8721860250c05c
DanielVillasmil <dvillasmil08@gmail.com>
2020-06-22 11:17:41 -0400

    passed driver name test

--- src/src/test/java/driverTest.java
+++ src/src/test/java/driverTest.java
@@ -1,2 +1,16 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 public class driverTest {
+    private Driver driver1;
+
+
+    @Test
+    public void driverShouldHaveName(){
+        Driver name = new Driver("Dan");
+        String result = name.getName();
+        assertEquals("Dan", result);
+    }
+
 }
